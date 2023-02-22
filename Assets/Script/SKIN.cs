b5e141132cefd0788f8ecc2522bdc1fe45dd4664
sodaakim <83997634+sodaakim@users.noreply.github.com>
2023-02-22 13:32:44 +0900

    enhanced feature/#2
    애니메이션 추가 완료 : dynamic, original

--- Assets/Script/SKIN.cs
+++ Assets/Script/SKIN.cs
@@ -7,21 +7,23 @@ using System.Linq;
 
 public class SKIN : MonoBehaviour
 {
-    // : Test
-    // public Sprite Image;
-    // public int num = 1;
+    // PlayerPrefs : GameMode
+    // 1 : Original
+    // 2 : Classic
+    // 3 : Dynamic
 
-    // : Color
-    // public GameObject ColorPalette;
-    // public GameObject ColorPrefab;
+// ------------------------------------------------------------------------ //
 
-
+    // +++ Color +++ //
     public List<Color> Color_List = new List<Color>();
 
-    // : Anim
-    public List<Animation> Animation_List = new List<Animation>();
 
-    // : Skill
+    // +++ Animation +++ //
+    // public List<Animator> Animation_List = new List<Animator>();
+    public Animator controller;
+
+
+    // +++ SKILL +++ //    
     public void Skill()
     {
         return;
@@ -29,12 +31,11 @@ public class SKIN : MonoBehaviour
     
     void Start()
     {
-
-        // Send Color List information to GameReady Hub
-        // GameObject.Find("ReadyGame").GetComponent<GameReadyHub>().Color_List_Hub = new List<Color>();
-        // GameObject.Find("ReadyGame").GetComponent<GameReadyHub>().Color_List_Hub = Color_List.ToList();
-
-
+        if (PlayerPrefs.GetInt("GameMode") == 1 || PlayerPrefs.GetInt("GameMode") == 3)     // Original, Dynamic
+        {
+            controller = gameObject.GetComponent<Animator>();
+            controller.runtimeAnimatorController = Resources.Load("SKIN_Anim/"+"Controller_Hop1") as RuntimeAnimatorController; 
+        }
     }
 
     void Update()
