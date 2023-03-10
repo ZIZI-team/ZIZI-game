25a2a6ec8a7afddf3c1ef2f7d4da87a5bc132bce
sodaakim <83997634+sodaakim@users.noreply.github.com>
2023-03-10 17:17:19 +0900

    enhanced feature/#4
    애니메이션 고쳤다!
    지지 움직임 가능한 위치 고려
    mode 선택 기능 추가해야함

--- Assets/Script/Title Scene/ZIZIAnim.cs
+++ Assets/Script/Title Scene/ZIZIAnim.cs
@@ -37,19 +37,57 @@ public class ZIZIAnim : MonoBehaviour
 
     public void OffAnim() // End of L, R, U, D
     { 
-        ZIZIRect.localPosition = new Vector3(0f, 0f, 0f);  
-        ZIZIcontroller.SetBool("Empty", false);
+        // ZIZIRect.localPosition = new Vector3(0f, 0f, 0f);  
+        // ZIZIcontroller.SetBool("Empty", false);
         Debug.Log("OffAnim");
     }
 
-    // End of L, R, U, D
-    public void Go_UP() { ZIZIParentRect.localPosition += new Vector3(0f, 200f, 0f);  Debug.Log("Up"); }
-    
-    public void Go_Down() { ZIZIParentRect.localPosition += new Vector3(0f, -200f, 0f);  Debug.Log("Down"); }
-    
-    public void Go_Right() { ZIZIParentRect.localPosition += new Vector3(200f, 0f, 0f);  Debug.Log("Right"); }
-    
-    public void Go_Left() { ZIZIParentRect.localPosition += new Vector3(-200f, 0f, 0f);  Debug.Log("Left"); }
+    // Animation ++++ End of L, R, U, D
+    int index_UpPos = 0;    
+    List<float> UpPos = new List<float>(){ 0f, 0.58997f, -25.606f, -43.743f, -49.062f, 3.7431f, 115.57f, 137f, 226.1f, 219f, 192.72f, 176.89f, 165.58f, 157f, 200f };
+    public void Go_UP() { 
+        ZIZIParentRect.localPosition += new Vector3(0f, UpPos[index_UpPos + 1] - UpPos[index_UpPos] , 0f); 
+        index_UpPos++; 
+        if (index_UpPos >= UpPos.Count - 1){ index_UpPos = 0; }
+    }   
+
+    int index_DownPos = 0;    
+    List<float> DownPos = new List<float>(){ 0f, -35.421f, -25.192f, -23.11f, -23.396f, 0f, 11.7f, -63f, -84.434f, -210.9f, -224f, -222f, -204.4f, -199.41f, -200f };
+    public void Go_Down() { 
+        ZIZIParentRect.localPosition += new Vector3(0f, DownPos[index_DownPos + 1] - DownPos[index_DownPos] , 0f); 
+        index_DownPos++; 
+        if (index_DownPos >= DownPos.Count - 1){ index_DownPos = 0; }
+        Debug.Log(DownPos.Count );
+    } 
+
+    int index_RightPosX = 0;  
+    int index_RightPosY = 0;  
+    List<float> RightPosX = new List<float>(){ 0f, -1f, 81.2f, 161.5f, 213.3f, 200f };
+    List<float> RightPosY = new List<float>(){ 0f, -5.722f, 61.3f, 64.3f, -3.8744f, 0f };
+
+    public void Go_Right() { 
+        ZIZIParentRect.localPosition += new Vector3(RightPosX[index_RightPosX + 1] - RightPosX[index_RightPosX], RightPosY[index_RightPosY + 1] - RightPosY[index_RightPosY], 0f); 
+        index_RightPosX++; 
+        index_RightPosY++; 
+        if (index_RightPosX >= RightPosX.Count - 1){ index_RightPosX = 0; index_RightPosY = 0; }
+        Debug.Log(RightPosX.Count );
+        Debug.Log(RightPosY.Count );
+        
+        Debug.Log("R : " + ZIZIParentRect.localPosition + " " + index_RightPosX + " " + index_RightPosY);
+    } 
+
+    int index_LeftPosX = 0;    
+    int index_LeftPosY = 0;
+    List<float> LeftPosX = new List<float>(){ 0f, -1f, -81.2f, -161.5f, -213.3f, -200f };
+    List<float> LeftPosY = new List<float>(){ 0f, -5.722f, 61.3f, 64.3f, -3.8744f, 0f };
+
+    public void Go_Left() { 
+        ZIZIParentRect.localPosition += new Vector3(LeftPosX[index_LeftPosX + 1] - LeftPosX[index_LeftPosX], LeftPosY[index_LeftPosY + 1] - LeftPosY[index_LeftPosY], 0f); 
+        index_LeftPosX++; 
+        index_LeftPosY++; 
+        if (index_LeftPosX >= LeftPosX.Count - 1){ index_LeftPosX = 0; index_LeftPosY = 0; }
+        Debug.Log("L : " + ZIZIParentRect.localPosition + " " + index_LeftPosX + " " + index_LeftPosY);
+    } 
 
 
     public void Stop() // End of Stop
